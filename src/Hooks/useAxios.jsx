12021f88f1d2a0a0bf6ba793a909c442cb4b2670
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-09 00:42:58 +0600

    complete project for now. will be update later

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const axiosIns = axios.create({
-  baseURL: `http://localhost:5000`,
+  baseURL: `https://pro-fast.vercel.app`,
 });
 
 const useAxios = () => {
